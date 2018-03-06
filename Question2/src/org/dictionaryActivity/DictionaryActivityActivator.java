bf24205c1ad4df69c7344f23fca7315cb81e1172
Thomas Faget <thomas.faget@hotmail.fr>
2018-03-05 23:34:59 -0500

    Application now looks for IDictionaryService instead of Dictionary to collect the services (+update bundle)

--- Question2/src/org/dictionaryActivity/DictionaryActivityActivator.java
+++ Question2/src/org/dictionaryActivity/DictionaryActivityActivator.java
@@ -1,11 +1,13 @@
 package org.dictionaryActivity;
 
-import org.dictionaryService.DictionaryService;
 import org.dictionaryService.able.IDictionaryService;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /** A bundle to launch a Dictionary application
  */
@@ -28,22 +30,23 @@ public class DictionaryActivityActivator implements BundleActivator, DictionaryS
 
     @Override
     public IDictionaryService[] findAllDictionaryServices() {
-        ServiceReference[] references = new ServiceReference[0];
+
+        List<ServiceReference<IDictionaryService>> references = null;
         try {
             // Find all the instance of DictionaryActivity started as services
-            references = context.getAllServiceReferences(DictionaryService.class.getName(), null);
+            references = new ArrayList<>(context.getServiceReferences(IDictionaryService.class, null));
         } catch (InvalidSyntaxException e) {
             e.printStackTrace();
         }
-        if (references == null || references.length == 0) {
+        if (references == null || references.size() == 0) {
             return new IDictionaryService[0];
         }
         else {
             // Extract all the DictionaryService found from the references
-            IDictionaryService[] services = new IDictionaryService[references.length];
-            for (int i = 0; i < references.length; i++) {
-                ServiceReference reference = references[i];
-                services[i] = (IDictionaryService) context.getService(reference);
+            IDictionaryService[] services = new IDictionaryService[references.size()];
+            for (int i = 0; i < references.size(); i++) {
+                ServiceReference<IDictionaryService> reference = references.get(i);
+                services[i] = context.getService(reference);
             }
             return services;
         }
