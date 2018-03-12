237e8becf205d18fcbb6dfeafd41d12065f4e006
Thomas Faget <thomas.faget@hotmail.fr>
2018-03-12 13:53:16 -0400

    add controller for the form

--- Question3/src/com/uqac/controller/BankController.java
+++ Question3/src/com/uqac/controller/BankController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.xml.transform.TransformerException;
+import javax.xml.ws.Action;
 
 @Controller
 public class BankController {
@@ -16,7 +17,7 @@ public class BankController {
     private Dal dal;
 
     @GetMapping(value = "/bank")
-    public ModelAndView displayAccount() {
+    public String displayAccount(ModelMap model) {
 
         if (dal == null) 
         {
@@ -30,33 +31,29 @@ public class BankController {
         		dal.getItem("service", id), 
         		Double.parseDouble(dal.getItem("solde", id)));
 
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("bank");
-        modelAndView.addObject("account", account);
+        model.addAttribute("account", account);
+        model.addAttribute("accountAction", new AccountAction());
 
-        return modelAndView;
-    }
-    
-    @ModelAttribute("accountAction")
-    public AccountAction formBackingObject() {
-        return new AccountAction();
+        return "bank";
     }
 
-    @PostMapping(value="/bankPost")
-    public String accountAction(@ModelAttribute("accountAction") AccountAction accountAction, BindingResult result, ModelMap model) {
-
-    	if (result.hasErrors()) 
-    	{
-            return "error";
-        }
+    @RequestMapping(value = "/accountAction", method = RequestMethod.POST)
+    public String accountAction(@ModelAttribute("accountAction") AccountAction accountAction) {
         
         if (dal == null) 
         {
             dal = new Dal();
-        }     	        
-        
-        //dal.deposer(id, accountAction.getValue());       
+        }
 
-        return "redirect:bank.html";
+        try {
+            if (accountAction.getAction().equals("deposer"))
+                dal.deposer(id, accountAction.getValue());
+            else if (accountAction.getAction().equals("retirer"))
+                dal.retirer(id, accountAction.getValue());
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/bank";
     }
 }
